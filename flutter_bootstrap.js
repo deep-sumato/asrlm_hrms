832e56c39b7e060e18df2b0ee44a982919454765
Deepjyoti <deepjyoti.sumatoglobal@gmail.com>
2025-11-01 17:44:11 +0530

    fixed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1316482876"
+    serviceWorkerVersion: "2409585947"
   }
 });
