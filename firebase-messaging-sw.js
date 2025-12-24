f25f1ba4e0d0580112bb19e79a32094ef23cf91f
Deepjyoti <deepjyoti.sumatoglobal@gmail.com>
2025-12-24 15:59:40 +0530

    notification fixed

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCdO0gJq9pddwtH2Ixwmh2ZyBH7b-yAF9I',
+    appId: '1:160379888343:web:1acd2f66599033954f8692',
+    messagingSenderId: '160379888343',
+    projectId: 'sumato-1bef7',
+    authDomain: 'sumato-1bef7.firebaseapp.com',
+    storageBucket: 'sumato-1bef7.firebasestorage.app',
+    measurementId: 'G-8HGBZTHYJH',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage((message) => {
+    console.log("onBackgroundMessage", message);
+});
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0876a60ed4bb1de7abe51c3cd8b570da",
+const RESOURCES = {"flutter_bootstrap.js": "5a04bf179e895780c58e19ac954cc66f",
 "version.json": "3d10d952ae8c179478adb40cc3de2cdd",
 "index.html": "e240b66c7eed751bfb7e5e9f68ea637f",
 "/": "e240b66c7eed751bfb7e5e9f68ea637f",
-"main.dart.js": "110de658e882c1dee5431b5ce409d059",
+"firebase-messaging-sw.js": "887f86d9b8fdb5ae69cdf42ca9feae70",
+"main.dart.js": "db808866892abb95d83d57ecdf3ced93",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "ac521209eec9586e5d47cde20b6fe246",
 "icons/Icon-192.png": "ff909bee8217fa46e9358fd9d477c8c7",
